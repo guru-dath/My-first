3bb6ce1c9e643aaec299794428a1e3038345e2cc
Gurudath K P <gurudath2017@gmail.com>
2023-01-08 15:54:26 +0000

    commiting in child branch

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 Public static void main(String args[])
 {
-System.out.println(“Hello Happly new year Gurufrom github”);
+System.out.println(“Hello Happly new year Gurufrom github from child ”);
 }
 }
 
